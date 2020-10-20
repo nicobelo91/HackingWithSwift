e51ab9f9b6b701ac5be19e61118fe0087493991e
Nico Cobelo <ingcobelo@gmail.com>
2020-10-20 08:52:32 -0400

    Initial commit

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by user183145 on 10/19/20.
+//  Copyright © 2020 Nico Cobelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                //this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    //                      how many rows there are in the table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    //               tableview is making the request
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        //gives the text label the same name as the picture in the cell
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
