037096359a86b3bedcf5b6c82c2ccf36151a77b9
Nico Cobelo <ingcobelo@gmail.com>
2020-10-22 19:52:56 -0400

    Added project2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by user183145 on 10/22/20.
+//  Copyright © 2020 Nico Cobelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
