ccdb8cb9233631ea321275d0b56cfa1a4ab5b3e1
Nico Cobelo <ingcobelo@gmail.com>
2020-10-25 20:06:12 -0400

    Added score to navigation bar, variable to keep track of questions and changed alert message when answer is wrong

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var questionNumber = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,22 +40,34 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        title = "\(countries[correctAnswer].uppercased())? - SCORE[\(score)] - \(questionNumber)/10"
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong, that's the flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
+        questionNumber += 1
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        if questionNumber < 10 {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         
-        present(ac, animated: true)
+            present(ac, animated: true)
+            
+        } else if questionNumber == 10 {
+            let ac = UIAlertController(title: "Final score", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+            questionNumber = 1
+            score = 0
+        }
     }
     
     
